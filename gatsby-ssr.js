9a702d592153948c33f529b35dc7c1611f15acd6
Bruno Chirelli <brunochirelli@gmail.com>
2020-07-25 23:05:30 -0300

    add root root css reset and clean the template

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,9 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
+import React from 'react';
+import { CssBaseline } from '@material-ui/core';
 
-// You can delete this file if you're not using it
+export const wrapRootElement = ({ element }) => (
+  <>
+    <CssBaseline />
+    {element}
+  </>
+);
