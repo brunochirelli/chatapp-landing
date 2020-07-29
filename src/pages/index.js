b2894169e3dfc24e3eb3d12efeff65ad2ede57fc
Bruno Chirelli <brunochirelli@gmail.com>
2020-07-29 10:49:15 -0300

    split landing in their own components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,47 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import styled from 'styled-components';
 
-import Layout from '../components/layout';
-import SEO from '../components/seo';
+import { Button } from 'gatsby-theme-material-ui';
+import { Typography, Container, Grid, Box, TextField } from '@material-ui/core';
+import { CtaButton, Section } from '../styles/components';
+
+import Layout from '../components/layout/layout';
+import SEO from '../components/layout/seo';
+
+import HeroBanner from '../components/HeroBanner';
+import CoreFeatures from '../components/CoreFeatures';
+import CreateAccountAside from '../components/CreateAccountAside';
+import VideoFeature from '../components/VideoFeature';
+import SubscribeSection from '../components/SubscribeSection';
+import PricingPlanSection from '../components/PricingPlanSection';
+import InviteFriendAside from '../components/InviteFriendAside';
+
+const WrapperStyled = styled.div`
+    section,
+    aside {
+        margin: 2.5rem auto;
+    }
+
+    .features-icon {
+        margin: 1.5rem 0;
+    }
+
+    .top-banner {
+        margin: 0 auto;
+    }
+`;
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-  </Layout>
+    <WrapperStyled>
+        <SEO title="Home" />
+        <HeroBanner />
+        <CoreFeatures />
+        <CreateAccountAside />
+        <InviteFriendAside />
+        <PricingPlanSection />
+        <VideoFeature />
+        <SubscribeSection />
+    </WrapperStyled>
 );
 
 export default IndexPage;
