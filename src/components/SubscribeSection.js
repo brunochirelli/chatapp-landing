37493d48ba43570058d6b3759aadc240ebaa5ca3
Bruno Chirelli <brunochirelli@gmail.com>
2020-07-31 08:02:57 -0300

    add grid for the section

--- src/components/SubscribeSection.js
+++ src/components/SubscribeSection.js
@@ -1,20 +1,32 @@
 import React from 'react';
+import styled from 'styled-components';
+
 import { Typography, Container, Grid } from '@material-ui/core';
-import { Button } from 'gatsby-theme-material-ui';
-import { Aside } from '../styles/components';
+import { Aside, CtaButton } from '../styles/components';
+
+const Wrapper = styled(Container)`
+    margin-top: 10rem;
+`;
 
 const SubscribeSection = () => (
-    <Container component={Aside}>
-        <Typography variant="h2" component="h2">
-            Subscribe Newsletter for Keep Updated
-        </Typography>
-        <Typography>
-            But I must explain to you how all this mistaken idea of denouncing
-            pleasure and praising pain was born and I complete account of the
-            system, and expound the actual teachings.
-        </Typography>
-        <Button variant="contained">Subscribe</Button>
-    </Container>
+    <Wrapper component={Aside}>
+        <Grid container>
+            <Grid item xs={12} sm={8}>
+                <Typography variant="h2" component="h2" paragraph tabIndex={0}>
+                    Subscribe Newsletter for Keep Updated
+                </Typography>
+                <Typography>
+                    But I must explain to you how all this mistaken idea of
+                    denouncing pleasure and praising pain was born and I
+                    complete account of the system, and expound the actual
+                    teachings.
+                </Typography>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+                <CtaButton variant="contained">Subscribe</CtaButton>
+            </Grid>
+        </Grid>
+    </Wrapper>
 );
 
 export default SubscribeSection;
